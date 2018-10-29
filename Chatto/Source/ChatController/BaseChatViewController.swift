10064d49377cd576d77abba29fef33275b5b7f13
Josie <josiewang92323@gmail.com>
2018-10-29 19:45:50 +0800

    Making isFirstLayout editable

--- Chatto/Source/ChatController/BaseChatViewController.swift
+++ Chatto/Source/ChatController/BaseChatViewController.swift
@@ -236,7 +236,7 @@ open class BaseChatViewController: UIViewController, UICollectionViewDataSource,
     var notificationCenter = NotificationCenter.default
     var keyboardTracker: KeyboardTracker!
 
-    public private(set) var isFirstLayout: Bool = true
+    public var isFirstLayout: Bool = true
     override open func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
